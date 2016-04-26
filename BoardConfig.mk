6cd36568009c2ab434acc641ff66efdab7eab3b2
Thomas Patry <thompatry@gmail.com>
2016-04-26 12:15:23 -0500

    Change from Samsung proprietary lpm charging to android universal charging
    Change-Id: Iae1d403da8acf3b5f8a65cdbbbaa7acbbb4c2fd0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,7 +62,7 @@ TARGET_KERNEL_SOURCE := kernel/samsung/exynos5420
 BOARD_CHARGER_SHOW_PERCENTAGE := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
-BOARD_BATTERY_DEVICE_NAME := batteryi
+BOARD_BATTERY_DEVICE_NAME := battery
 BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
 
 # Bootloader
