0bb0834f0a7ed548e123c958a57f7cd6c0d9d9d4
deadman96385 <seanhoyt963@gmail.com>
2016-11-08 10:27:52 -0600

    Use Exynos5420-common
    Change-Id: I842d29d25a70d8eb550cb8ec403e06c76e8b80fd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,113 +17,20 @@
 LOCAL_PATH := device/samsung/v1awifi
 
 # Platform
-BOARD_VENDOR := samsung
 TARGET_BOARD_PLATFORM := exynos5
-TARGET_SLSI_VARIANT := cm
 TARGET_SOC := exynos5420
 
-# Architecture
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a15
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_SMP := true
-TARGET_CPU_VARIANT := cortex-a15
-
-# Vold
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun%d/file
-
 # Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_HAVE_SAMSUNG_BLUETOOTH := true
-BOARD_BLUEDROID_VENDOR_CONF := $(LOCAL_PATH)/bluetooth/libbt_vndcfg.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 
 # Bootloader
 TARGET_OTA_ASSERT_DEVICE := v1awifi
 
-# Camera
-BOARD_CAMERA_SNUMINTS := 34
-BOARD_NEEDS_MEMORYHEAPION := true
-COMMON_GLOBAL_CFLAGS += -DCAMERA_SNUMINTS=$(BOARD_CAMERA_SNUMINTS)
-COMMON_GLOBAL_CFLAGS += -DSAMSUNG_DVFS
-
-# Force the screenshot path to CPU consumer
-COMMON_GLOBAL_CFLAGS += -DFORCE_SCREENSHOT_CPU_PATH
-
 # Kernel
-BOARD_KERNEL_BASE := 0x10000000
-BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_CONFIG := cyanogenmod_deathly_v1awifi_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/exynos5420
-
-# Battery
-BOARD_CHARGER_SHOW_PERCENTAGE := true
-BOARD_CHARGER_ENABLE_SUSPEND := true
-BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
-BOARD_BATTERY_DEVICE_NAME := battery
-BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
-
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := universal5420
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-
-# FIMG2D
-BOARD_USES_SKIA_FIMGAPI := true
-
-# Graphics
-USE_OPENGL_RENDERER := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-TARGET_BOOTANIMATION_MULTITHREAD_DECODE := true
-
-# Renderscript
-BOARD_OVERRIDE_RS_CPU_VARIANT_32 := cortex-a53
-
-# Exynos display
-BOARD_USES_VIRTUAL_DISPLAY := true
-
-# HWCServices
-BOARD_USES_HWC_SERVICES := true
-
-# Keymaster
-BOARD_USES_TRUST_KEYMASTER := true
-
-# Extended filesystem support
-TARGET_KERNEL_HAVE_EXFAT := true
-TARGET_KERNEL_HAVE_NTFS := true
-
-# Include path
-TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # IR Blaster
 IR_HAS_ONE_FREQ_RANGE := true
-
-# Hardware
-BOARD_HARDWARE_CLASS += device/samsung/v1awifi/cmhw
-BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
-
-# Media
-COMMON_GLOBAL_CFLAGS += -DUSE_NATIVE_SEC_NV12TILED # use format from fw/native
-COMMON_GLOBAL_CFLAGS += -DWIDEVINE_PLUGIN_PRE_NOTIFY_ERROR
-
-# OpenMAX Video
-BOARD_USE_STOREMETADATA := true
-BOARD_USE_METADATABUFFERTYPE := true
-BOARD_USE_DMA_BUF := true
-BOARD_USE_ANB_OUTBUF_SHARE := true
-BOARD_USE_IMPROVED_BUFFER := true
-BOARD_USE_NON_CACHED_GRAPHICBUFFER := true
-BOARD_USE_GSC_RGB_ENCODER := true
-BOARD_USE_CSC_HW := true
-BOARD_USE_QOS_CTRL := false
-BOARD_USE_S3D_SUPPORT := true
-BOARD_USE_VP8ENC_SUPPORT := true
-
-# Sensors
-TARGET_NO_SENSOR_PERMISSION_CHECK := true
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
@@ -133,52 +40,8 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2300000000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 28378660864
 BOARD_FLASH_BLOCK_SIZE := 4096
 
-# PowerHAL
-TARGET_POWERHAL_VARIANT := samsung
-
-# Enable dex-preoptimization to speed up first boot sequence
-WITH_DEXPREOPT := true
-
-# Disable journaling on system.img to save space
-BOARD_SYSTEMIMAGE_JOURNAL_SIZE := 0
-
-# Recovery
-COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
-BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.universal5420
-TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_RECOVERY_SWIPE := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-
-# Samsung Gralloc
-TARGET_SAMSUNG_GRALLOC_EXTERNAL_USECASES := true
-
-# Scaler
-BOARD_USES_SCALER := true
-BOARD_USES_GSC_VIDEO := true
-
 # SELinux
-BOARD_SEPOLICY_DIRS += \
-    device/samsung/v1awifi/sepolicy
+BOARD_SEPOLICY_DIRS += device/samsung/exynos5420-common/sepolicy
 
-# Webkit
-ENABLE_WEBGL := true
-
-# WFD
-BOARD_USES_WFD := true
-
-# Wifi
-BOARD_HAVE_SAMSUNG_WIFI          := true
-BOARD_WLAN_DEVICE                := bcmdhd
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
-WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
-
-# inherit from the proprietary version
--include vendor/samsung/v1awifi/BoardConfigVendor.mk
+# Inherit from exynos5420-common
+include device/samsung/exynos5420-common/BoardConfigCommon.mk
---
+++ ramdisk/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.target.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= init.target.rc
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= ueventd.universal5420.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= ueventd.universal5420.rc
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
