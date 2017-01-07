74301bde910b41192b7856506386d440403608b8
Thomas Patry <thompatry@gmail.com>
2017-01-07 17:27:34 -0600

    Revert "Rebrand to LineageOS"
    Lineage is not rebranding the CM-13.0 branch.
    
    This reverts commit 5b1432dd8ade616b07b6941a1ef4271c3713f726.
    
    Change-Id: I666caee8a782bc42a426cb3816c17da87195cf17

--- cm.mk
+++ cm.mk
@@ -10,10 +10,10 @@ PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := SM-P900
 
-# Inherit some common LineageOS stuff.
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_NAME := lineage_v1awifi
+PRODUCT_NAME := cm_v1awifi
 PRODUCT_DEVICE := v1awifi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
