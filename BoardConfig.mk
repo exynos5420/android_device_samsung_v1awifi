d3a467dc8f04a4f4f95844879cb6be0977c664e9
Martin Bouchet <tincho5588@gmail.com>
2017-01-12 02:43:50 -0500

    BoardConfig: remove the BOARD_CUSTOM_BOOTIMG flag
    Change-Id: I6c4b26533d97feae92dd8e07bc45733587b118ee

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,6 @@ TARGET_OTA_ASSERT_DEVICE := v1awifi
 
 # Kernel
 TARGET_KERNEL_CONFIG := liquiddeathos_deathly_v1awifi_defconfig
-BOARD_CUSTOM_BOOTIMG := true
 
 # IR Blaster
 IR_HAS_ONE_FREQ_RANGE := true
