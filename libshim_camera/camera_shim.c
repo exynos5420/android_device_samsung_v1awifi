279bae54fa3f27ae67eee46258fe8dfdcd474063
Thomas Patry <thompatry@gmail.com>
2018-08-24 01:46:30 -0400

    DO NOT MERGE: v1awifi: Camera bring up to Oreo
    Change-Id: I74c7cdfdd8ef4f2d31c753ea8f0d2b08e8773697

---
+++ libshim_camera/camera_shim.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const char _ZN7android16CameraParameters8ISO_AUTOE[] = "auto";
+const char _ZN7android16CameraParameters7ISO_100E[] = "100";
+const char _ZN7android16CameraParameters7ISO_200E[] = "200";
+const char _ZN7android16CameraParameters7ISO_400E[] = "400";
+const char _ZN7android16CameraParameters7ISO_800E[] = "800";
+
+const char _ZN7android16CameraParameters17EFFECT_CARTOONIZEE[] = "cartoonize";
+const char _ZN7android16CameraParameters23EFFECT_POINT_RED_YELLOWE[] = "point-red-yellow";
+const char _ZN7android16CameraParameters18EFFECT_POINT_GREENE[] = "point-green";
+const char _ZN7android16CameraParameters17EFFECT_POINT_BLUEE[] = "point-blue";
+const char _ZN7android16CameraParameters19EFFECT_VINTAGE_COLDE[] = "vintage-cold";
+const char _ZN7android16CameraParameters19EFFECT_VINTAGE_WARME[] = "vintage-warm";
+const char _ZN7android16CameraParameters13EFFECT_WASHEDE[] = "washed";
+
+const char _ZN7android16CameraParameters10KEY_CITYIDE[] = "";
+
+const char _ZN7android16CameraParameters26PIXEL_FORMAT_YUV420SP_NV21E[] = "nv21";
+
+int _ZNK7android16CameraParameters8getInt64EPKc(const char *key) { return -1; }
