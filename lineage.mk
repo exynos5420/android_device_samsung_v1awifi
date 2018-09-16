d4dd04f4c2fbc8deb06f8790aaa1c0eafd7a7941
Vasyl Gello <vasek.gello@gmail.com>
2018-09-16 21:15:31 +0300

    v1awifi: Set build fingerprint from latest stock
    This allows passing SafetyNet profile check.
    
    Change-Id: I08b49431df1406cf745fc15af44a4121e636ef5a
    (cherry picked from commit 34eeb395cf008f3b48fd2c70e0052c68457dc404)

---
+++ lineage.mk
@@ -0,0 +1,29 @@
+<<<<<<< HEAD   (28b46a v1awifi: Use TARGET_OUT_VENDOR/TARGET_COPY_OUT_SYSTEM)
+=======
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from v1awifi device
+$(call inherit-product, device/samsung/v1awifi/device.mk)
+
+PRODUCT_NAME := full_v1awifi
+PRODUCT_DEVICE := v1awifi
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SM-P900
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+
+PRODUCT_NAME := lineage_v1awifi
+PRODUCT_DEVICE := v1awifi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_MODEL=SM-P900 \
+    PRODUCT_NAME=v1awifi \
+    PRODUCT_DEVICE=v1awifi \
+    PRIVATE_BUILD_DESC="samsung/v1awifixx/v1awifi:5.0.2/LRX22G/P900XXU0BOI1:user/release-keys" \
+    TARGET_DEVICE=v1awifi
+
+BUILD_FINGERPRINT := samsung/v1awifixx/v1awifi:5.0.2/LRX22G/P900XXU0BOI1:user/release-keys
+>>>>>>> CHANGE (34eeb3 v1awifi: Set build fingerprint from latest stock)
