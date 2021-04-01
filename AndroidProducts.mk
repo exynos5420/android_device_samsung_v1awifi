exynos5420/android_device_samsung_n1awifi

v1awifi: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

AndroidProducts.mk
@@ -18,3 +18,8 @@ LOCAL_PATH := device/samsung/n1awifi
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_n1awifi.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_n1awifi-user \
+    lineage_n1awifi-userdebug \
+    lineage_n1awifi-eng

exynos5420/android_device_samsung_v1awifi

AndroidProducts.mk
@@ -18,3 +18,8 @@ LOCAL_PATH := device/samsung/v1awifi
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_v1awifi.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_v1awifi-user \
+    lineage_v1awifi-userdebug \
+    lineage_v1awifi-eng